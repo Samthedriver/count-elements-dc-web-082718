d0a9142082d23a2a96a8a48b7af549eb46ed02ba
github username <github email address>
2018-08-28 12:48:07 -0400

    Defined method to count elements and return hash.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
   # code goes here
+  hash_string = Hash.new
+  array.each do |string|
+    if hash_string[string] != nil
+      hash_string[string] += 1
+    else
+      hash_string[string] = 1
+    end
+  end
+  hash_string
 end
- 
